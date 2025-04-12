0ea5575c1c3133fb200485991478c9c667ff8776
Piyush Channawar <channawarpiyush@gmail.com>
2025-04-12 22:53:37 +0530

    Refactor caching functions to replace 'mean' with 'solve' for matrix inversion

--- week3.r
+++ week3.r
@@ -1,31 +1,34 @@
-setwd('C:/Users/rubind1/Documents/Coursera-R')
-##
-## I simply set the input x as a matrix
-## and then set the solved value "s" as a null
-## then I changed every reference to "mean" to "solve"
-makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
-  s <- NULL
-  set <- function(y) {
-    x <<- y
-    s <<- NULL
+## Put contents here that give an overall description of what your
+## functions do
+
+##   The following function creates a 'special matrix' that helps saving the value of the inverse of a squared matrix
+## without the need to re-calculate it. makeCacheMatrix returns a list of 4 functions that allow setting/getting the 
+## values of the matrix & its inverse
+
+makeCacheMatrix <- function(mat=matrix()){
+  inv<-NULL
+  set <- function(mat2) {
+    mat <<- mat2
+    inv <<- NULL
   }
-  get <- function() x
-  setsolve <- function(solve) s <<- solve
-  getsolve <- function() s
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+  get <- function() mat
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
-##
-## Same here, changed "mean" to "solve" and "m" to "s"
-cacheSolve <- function(x, ...) {
-  s <- x$getsolve()
-  if(!is.null(s)) {
-    message("getting inversed matrix")
-    return(s)
+
+
+##   The following function calculates the inverse of the 'special matrix' if it's a new matrix & returns the previously
+## calculated inverse if it has already been calculated (its value is cached) 
+
+cacheSolve <- function(mat, ...) {
+  inv <- mat$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
   }
-  data <- x$get()
-  s <- solve(data, ...)
-  x$setsolve(s)
-  s
+  data <- mat$get()
+  inv <- solve(data, ...)
+  mat$setinv(inv)
+  inv
 }
